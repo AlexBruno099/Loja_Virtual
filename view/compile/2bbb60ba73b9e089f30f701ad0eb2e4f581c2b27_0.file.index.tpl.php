58eeb673312297198b19a8ba3e6c7d46532160ce
AlexBruno099 <alexbrunorr099@gmail.com>
2025-07-08 23:08:00 -0300

    feat: creating several pages and some treatments.

---
+++ view/compile/2bbb60ba73b9e089f30f701ad0eb2e4f581c2b27_0.file.index.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.46, created on 2025-07-09 03:54:16
+  from 'C:\xampp\htdocs\Loja_Virtual\view\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.46',
+  'unifunc' => 'content_686dcbc84b4389_33087957',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2bbb60ba73b9e089f30f701ad0eb2e4f581c2b27' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Loja_Virtual\\view\\index.tpl',
+      1 => 1752025618,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_686dcbc84b4389_33087957 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+    Esta é a página carregada com o Smarty
+
+    <h2><?php echo $_smarty_tpl->tpl_vars['NOME']->value;?>
+</h2>
+</body>
+</html><?php }
+}
