438ac41b329a53c83c60223b4cdb8be8edac72f7
AlexBruno099 <alexbrunorr099@gmail.com>
2025-07-10 21:59:09 -0300

    feat: creating friendly URLs

--- lib/composer/installed.php
+++ lib/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'eeeba118ce939f74068f8e000902d7116728035b',
+        'reference' => '58eeb673312297198b19a8ba3e6c7d46532160ce',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'eeeba118ce939f74068f8e000902d7116728035b',
+            'reference' => '58eeb673312297198b19a8ba3e6c7d46532160ce',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
