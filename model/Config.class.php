af0bdb707bdb35a8be9d148d3a35322fa53d0ed9
AlexBruno099 <alexbrunorr099@gmail.com>
2025-07-14 22:02:57 -0300

    feat: create configs.

--- model/Config.class.php
+++ model/Config.class.php
@@ -1,8 +1,20 @@
 <?php 
+class Config(){
+    // INFORMAÇÕES BASICAS DO SITE
+    CONST SITE_URL = "http://localhost";
+    const SITE_PASTA = "Loja_virtual";
+    const SITE_NOME = "Loja do Alex - PHP e Mysqli";
+    const SITE_EMAIL_ADM = "alexbrunorr099@gmail.com";
 
-const SITE_URL = "http://localhost";
-const SITE_PASTA = "Loja_virtual";
-const SITE_NOME = "Loja do Alex - PHP e Mysqli";
-const SITE_EMAIL_ADM = "alexbrunorr099@gmail.com";
+    //INFORMAÇÕES DO BANCO DE DADOS
+    const BD_HOST = "localhost",
+          BD_USER = "root",
+          BD_SENHA = "",
+          BD_BANCO = "lojaAlex";
+          
+// INFORMAÇÕES PARA PHP MAILLER
+    const EMAIL_HOST = "smtp.gmail.com";
 
+}
 ?>
+
